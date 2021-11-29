fed0f8bbd80c522e792716d26a89027a21cc0ba0
shawnelldh <shawnelldh@gmail.com>
2021-11-29 17:21:47 -0500

    3 files modified, 3 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
-import "./style.css";
+import { loadStripe } from "@stripe/stripe-js";
 
-export default function App() {
+const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
+
+
+
+import React from "react";
+import "./styles.css";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe("pk_test_35p114pH8oNuHX72SmrvsFqh00Azv3ZaIA");
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <div className="product">
+        <img
+          src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress"
+          alt="laptop"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <div>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default App;
